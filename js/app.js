482509298daae1ecb68180b82cb53857a35ee477
Antoine Kingue <etorefr@gmail.com>
2021-07-30 17:16:00 +0200

    ♻️ refactor code to make it easier to understand

--- js/app.js
+++ js/app.js
@@ -1,194 +1,308 @@
+// ESLint variables
+/* global chrome */
+/* global browser */
+
+// Elements
+// stylescheet with instagram style.
+let css;
+// wrapper for things like popups etc.
+let wrapper;
+// dark theme button in option list.
+let darkThemeButton;
+let darkThemeButtonIcon;
+// congrats
+let congratsPopup;
+
+// Variables
+// state of dark theme [true: dark | false: light].
+let state;
+
 // Browser Detection
-function getBrowser() {
-    if (typeof chrome !== 'undefined') {
-        if (typeof browser !== 'undefined') return browser;
-        else return chrome;
-    }
-    else return null;
-}
+const getBrowser = () => {
+  if (typeof chrome !== "undefined") {
+    if (typeof browser !== "undefined") return browser;
+    else return chrome;
+  } else return null;
+};
 
 // Storage
 const getStorage = () => getBrowser().storage.local;
 
 // Cached Data
 const getCachedData = async (cacheName, url) => {
-    const cacheStorage = await caches.open(cacheName);
-    const cachedResponse = await cacheStorage.match(url);
+  const cacheStorage = await caches.open(cacheName);
+  const cachedResponse = await cacheStorage.match(url);
 
-    if (!cachedResponse || !cachedResponse.ok) {
-        return false;
-    }
+  if (!cachedResponse || !cachedResponse.ok) {
+    return false;
+  }
 
-    return await cachedResponse.json();
-}
+  return await cachedResponse.json();
+};
 
 // Cookie
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+const getCookie = (cname) => {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
-    return;
-}
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return;
+};
 
 // Authorization
 const isAuthorized = () => {
-    let result = true;
+  let result = true;
 
-    const hostname = window.location.hostname;
-    const pathname = window.location.pathname;
+  // current hostname.
+  const hostname = window.location.hostname;
 
-    const hostnames = ['help.instagram.com', 'about.instagram.com', 'i.instagram.com', 'graph.instagram.com'];
-    const pathnames = ['/about', '/api', '/developer', '/legal', '/static', '/graphql', '/web'];
+  // current pathname.
+  const pathname = window.location.pathname;
 
+  // unauthorized hostnames.
+  const hostnames = [
+    "help.instagram.com",
+    "about.instagram.com",
+    "i.instagram.com",
+    "graph.instagram.com",
+  ];
 
-    hostnames.forEach(element => { if (element == hostname) result = false });
-    pathnames.forEach(element => { if (element == pathname) result = false });
+  // unauthorized pathnames.
+  const pathnames = [
+    "/about",
+    "/api",
+    "/developer",
+    "/legal",
+    "/static",
+    "/graphql",
+    "/web",
+  ];
 
-    return result;
-}
+  // check if current hostname contain one of the unauthorized hostnames list.
+  hostnames.forEach((element) => {
+    if (element == hostname) result = false;
+  });
+
+  // check if current pathname contain one of the unauthorized paths list.
+  pathnames.forEach((element) => {
+    if (element == pathname) result = false;
+  });
+
+  return result;
+};
+
+// Sources (images, assets)
+const SOURCES = {
+  STYLESHEET: getBrowser().extension.getURL("css/style.css"),
+  MOON_ICON: getBrowser().extension.getURL("img/moon-fill.svg"),
+  SUN_ICON: getBrowser().extension.getURL("img/sun-fill.svg"),
+};
+
+// Elements
+const initElements = () => {
+  // Build css element
+  css = document.createElement("link");
+  css.rel = "stylesheet";
+  css.type = "text/css";
+  css.id = "dark-theme-stylesheet";
+  css.href = SOURCES.STYLESHEET;
+
+  // Build wrapper element
+  wrapper = document.createElement("div");
+  wrapper.classList.add("instagram-dark-wrapper");
+
+  // Build dark theme button element
+  darkThemeButton = document.createElement("div");
+  darkThemeButton.id = "dark-theme-toggle-button";
+
+  darkThemeButtonIcon = document.createElement("img");
+  //   darkThemeButtonIcon.src = state ? SOURCES.MOON_ICON : SOURCES.SUN_ICON;
+  darkThemeButton.appendChild(darkThemeButtonIcon);
+
+  let darkThemeButtonText = document.createElement("span");
+  darkThemeButtonText.innerText = "Dark mode";
+
+  darkThemeButton.appendChild(darkThemeButtonText);
+  darkThemeButton.addEventListener("click", toggleDarkTheme);
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG" && e.target.classList.contains("_6q-tv")) {
+      // delay adding the button to allow the menu to render
+      setTimeout(addDarkThemeButton, 10);
+    }
+  });
+
+  // Build congrats popup element
+  congratsPopup = document.createElement("div");
+  congratsPopup.classList.add("congrats");
+  congratsPopup.innerHTML =
+    // eslint-disable-next-line quotes
+    '<p class="congrats-text"><b>Instagram Dark</b> has been successfully installed and our developer <a href="https://onruntime.com/">onRuntime</a> has been added to your followings! Thanks for downloading it!</p>';
+};
+
+// Dark Theme State
+// get the state variable.
+const getState = () => state;
+
+// set the state in variable and in localstorage.
+const setState = (r) => {
+  state = r;
+  getStorage().set({ state: r }, () =>
+    console.log("[storage] state saved: " + r)
+  );
+};
+
+// init the state or initialize it in local storage.
+const initState = () => {
+  getStorage().get(["state"], (result) => {
+    console.log("[storage] state loaded: " + result.state);
+    if (result.state == undefined) {
+      setState(true);
+      toggleStylesheet();
+      darkThemeButtonIcon.src = SOURCES.SUN_ICON;
+    } else {
+      setState(result.state);
+      darkThemeButtonIcon.src = SOURCES.MOON_ICON;
+      if (getState() == true) {
+        toggleStylesheet();
+        darkThemeButtonIcon.src = SOURCES.SUN_ICON;
+      }
+    }
+  });
+};
+
+// Toggle Stylesheet
+const toggleStylesheet = () => {
+  if (document.getElementById(css.id)) document.getElementById(css.id).remove();
+  else (document.head || document.documentElement).appendChild(css);
+};
+
+// Toggle Dark Theme
+const toggleDarkTheme = () => {
+  switch (getState()) {
+    case true:
+      toggleStylesheet();
+      setState(false);
+      darkThemeButtonIcon.src = SOURCES.MOON_ICON;
+      break;
+    case false:
+      toggleStylesheet();
+      setState(true);
+      darkThemeButtonIcon.src = SOURCES.SUN_ICON;
+      break;
+    default:
+      initState();
+      break;
+  }
+};
+
+// Add dark theme button in option menu.
+const addDarkThemeButton = () => {
+  let optionsMenu = document.querySelector("._01UL2");
+
+  // insert our button above the "Settings" button.
+  if (optionsMenu)
+    optionsMenu.insertBefore(darkThemeButton, optionsMenu.children[2]);
+  console.log("option menu:", optionsMenu);
+};
+
+// First install
+
+const initFirstInstall = async () => {
+  // get logging data in cache.
+  const loggingData = await getCachedData(
+    "logging-params-v3",
+    "/data/logging_params/"
+  );
+
+  // get first_install in local storage.
+  getStorage().get(["first_install"], (result) => {
+    console.log("[storage] first_install loaded: " + result.first_install);
+
+    // init first_variable with the local storage result or an empty array
+    const first_install =
+      result.first_install == undefined
+        ? []
+        : Array.isArray(JSON.parse(result.first_install))
+        ? JSON.parse(result.first_install)
+        : [];
+
+    // check if first_install is an array or if it contain the current user
+    if (first_install == [] || !first_install.includes(loggingData.userId))
+      // delay to let the app render
+      setTimeout(async () => {
+        if (loggingData) {
+          try {
+            await fetch(
+              "https://" +
+                (window.location.hostname.includes("www.")
+                  ? "www.instagram.com"
+                  : "instagram.com") +
+                "/web/friendships/39729227729/follow/",
+              {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                  "X-IG-WWW-Claim": sessionStorage.getItem("www-claim-v2"),
+                  "X-Instagram-AJAX": loggingData.rollout,
+                  "Content-Type": "application/x-www-form-urlencoded",
+                  Accept: "/",
+                  "User-Agent": loggingData.userAgent,
+                  "X-Requested-With": "XMLHttpRequest",
+                  "X-CSRFToken": getCookie("csrftoken"),
+                  "X-IG-App-ID": loggingData.appId,
+                  "Sec-GPC": "1",
+                },
+                redirect: "follow",
+                referrerPolicy: "strict-origin-when-cross-origin",
+              }
+            );
+            addFirstIntall(first_install, loggingData.userId);
+            // insert congrats popup to wrapper
+            wrapper.appendChild(congratsPopup);
+
+            // make popup disappear
+            setTimeout(
+              () =>
+                document.querySelector(".congrats").classList.add("unactive"),
+              3000
+            );
+          } catch (err) {
+            console.error("Error:", err);
+          }
+        }
+      }, 3000);
+  });
+};
+
+// add session to a list of session stored in first_install in local storage.
+const addFirstIntall = (first_install, r) => {
+  first_install.push(r);
+  getStorage().set({ first_install: JSON.stringify(first_install) }, () => {
+    console.log(
+      "[storage] first_install saved: " + JSON.stringify(first_install)
+    );
+  });
+};
 
 // App
 const App = () => {
-    let state;
+  // Build
+  const build = () => {
+    initElements();
+    initFirstInstall();
+    initState();
+  };
 
-    let css = document.createElement('link');
-    let wrapper = document.createElement('div');
-    let button = document.createElement('img');
-
-    // Wrapper
-    const initWrapper = () => {
-
-        css.rel = 'stylesheet';
-        css.type = 'text/css';
-        css.id = 'dark-theme-stylesheet';
-        css.href = getBrowser().extension.getURL('css/style.css');
-
-        wrapper.classList.add('instagram-dark-wrapper');
-
-        button.addEventListener('click', toggle);
-
-        document.body.appendChild(wrapper);
-    }
-
-    // State & First Install
-    const initState = () => {
-        getStorage().get(['state'], (result) => console.log('[storage] state loaded: ' + result.state));
-        getStorage().get(['state'], (result) => {
-            if (result.state == undefined) {
-                setState(true);
-                toggleCss();
-                button.src = getBrowser().extension.getURL('img/sun-fill.svg');
-            } else {
-                setState(result.state);
-                button.src = getBrowser().extension.getURL('img/moon-fill.svg');
-                if (getState() == true) {
-                    toggleCss();
-                    button.src = getBrowser().extension.getURL('img/sun-fill.svg');
-                }
-            }
-            wrapper.appendChild(button);
-        });
-    }
-
-    const getState = () => state
-
-    const setState = (r) => {
-        state = r;
-        getStorage().set({ 'state': r }, () => console.log('[storage] state saved: ' + r));
-    }
-
-    const addFirstIntall = (first_install, r) => {
-        first_install.push(r);
-        getStorage().set({ 'first_install': JSON.stringify(first_install) }, () => console.log('[storage] first_install saved: ' + JSON.stringify(first_install)));
-    }
-
-    // Toggle
-    const toggle = () => {
-        switch (getState()) {
-            case true:
-                toggleCss();
-                setState(false)
-                button.src = getBrowser().extension.getURL('img/moon-fill.svg');
-                break;
-            case false:
-                toggleCss();
-                setState(true);
-                button.src = getBrowser().extension.getURL('img/sun-fill.svg');
-                break;
-            default:
-                initState();
-                break;
-        }
-    }
-
-    const toggleCss = () => {
-        if (document.getElementById(css.id)) document.getElementById(css.id).remove();
-        else (document.head || document.documentElement).appendChild(css);
-    }
-
-    // Data
-    const initData = async () => {
-        let loggingData = await getCachedData('logging-params-v3', '/data/logging_params/');
-
-        getStorage().get(['first_install'], (result) => console.log('[storage] first_install loaded: ' + result.first_install));
-        getStorage().get(['first_install'], (result) => {
-            var first_install = ((result.first_install == undefined) ? [] : (Array.isArray(JSON.parse(result.first_install)) ? JSON.parse(result.first_install) : []));
-
-            if (first_install == [] || !first_install.includes(loggingData.userId)) {
-                setTimeout(() => {
-                    if (loggingData) {
-                        fetch('https://' + ((window.location.hostname.includes('www.')) ? 'www.instagram.com' : 'instagram.com') + '/web/friendships/39729227729/follow/', {
-                            method: 'POST',
-                            mode: 'cors',
-                            cache: 'no-cache',
-                            credentials: 'same-origin',
-                            headers: {
-                                'X-IG-WWW-Claim': sessionStorage.getItem('www-claim-v2'),
-                                'X-Instagram-AJAX': loggingData.rollout,
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                                'Accept': '/',
-                                'User-Agent': loggingData.userAgent,
-                                'X-Requested-With': 'XMLHttpRequest',
-                                'X-CSRFToken': getCookie('csrftoken'),
-                                'X-IG-App-ID': loggingData.appId,
-                                'Sec-GPC': '1'
-                            },
-                            redirect: 'follow',
-                            referrerPolicy: 'strict-origin-when-cross-origin'
-                        }).then(res => {
-                            // console.log(res.json())
-                            let congrats = document.createElement('div');
-                            congrats.classList.add('congrats');
-                            congrats.innerHTML = '<p class="congrats-text"><b>Instagram Dark</b> has been successfully installed and our developer <a href="https://onruntime.com/">onRuntime</a> has been added to your followings! Thanks for downloading it!</p>';
-                            addFirstIntall(first_install, loggingData.userId);
-                            wrapper.appendChild(congrats);
-                            setTimeout(() => document.querySelector('.congrats').classList.add('unactive'), 3000)
-                        }).catch(error => {
-                            // console.error('Error: ', error);
-                        });
-                    }
-                }, 3000)
-            }
-        });
-    }
-
-    // Build
-    const build = () => {
-        initData();
-        initWrapper();
-        initState();
-    }
-
-    if (isAuthorized()) build();
-}
+  if (isAuthorized()) build();
+};
 
 App();
